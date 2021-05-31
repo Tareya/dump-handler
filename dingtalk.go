b1e1b9a7cd452ea9bd9a2afcaf3d057f14f68632
沈磊 <shen.lei@uniondrug.com>
2021-05-31 10:38:19 +0800

    First Compile

--- dingtalk.go
+++ dingtalk.go
@@ -2,24 +2,24 @@ package main
 
 import (
 	"fmt"
-	"github.com/braumye/grobot"
 	"time"
+
+	"github.com/braumye/grobot"
 )
 
-
-func msgSender(dingdingToken string,msg string) {
+func msgSender(dingdingToken string, msg string) {
 	robot, _ := grobot.New("dingtalk", dingdingToken)
-	err := robot.SendMarkdownMessage("通知",msg)
-	fmt.Println("通知发送完毕",err)
+	err := robot.SendMarkdownMessage("通知", msg)
+	fmt.Println("通知发送完毕", err)
 }
 
 func alarm() {
 	//key=项目组，value=钉钉token [需要修改]
 	tokenMap := map[string]string{
-		"ops":"xxx",
+		"ops": "xxx",
 	}
 	dingdingToken = tokenMap[projectGroup]
-	ossurl := fmt.Sprintf("http://%s.oss-cn-shanghai-internal.aliyuncs.com/%s/k8s/jvm/%s/%s-%s", bucketName,env,folder,podId,postfix) //建议OSS内网，"oss-cn-shanghai-internal"更换成自己的endpoint[需要修改]
-	alarmMsg := fmt.Sprintf("<font color=#FF0000 size=5 face='黑体'>事故警告: JVM OOM</font>\n### 服务名: %s\n### 时间: %s\n### Dump文件: %s",podId,time.Now().Format("2006/01/02 15:04:05"),ossurl)
-	msgSender(dingdingToken,alarmMsg)
+	ossurl := fmt.Sprintf("http://%s.oss-cn-hangzhou-internal.aliyuncs.com/jvm/%s/%s/%s-%s", bucketName, env, folder, podId, postfix) //建议OSS内网，"oss-cn-shanghai-internal"更换成自己的endpoint[需要修改]
+	alarmMsg := fmt.Sprintf("<font color=#FF0000 size=5 face='黑体'>事故警告: JVM OOM</font>\n### 服务名: %s\n### 时间: %s\n### Dump文件: %s", podId, time.Now().Format("2006/01/02 15:04:05"), ossurl)
+	msgSender(dingdingToken, alarmMsg)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module dump-handler
 
 go 1.12
+
+require (
+	github.com/aliyun/aliyun-oss-go-sdk v2.1.8+incompatible
+	github.com/braumye/grobot v1.0.1
+	github.com/ramya-rao-a/go-outline v0.0.0-20200117021646-2a048b4510eb // indirect
+	golang.org/x/time v0.0.0-20210220033141-f8bda1e9f3ba // indirect
+	golang.org/x/tools v0.1.2 // indirect
+)
